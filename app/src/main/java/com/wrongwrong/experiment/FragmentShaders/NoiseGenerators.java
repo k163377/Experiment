91194aae24ead3f0b7a515126c9ba0a88b7f8f79
k163377 <k163377@edu.tut.ac.jp>
2017-10-21 05:32:35 +0900

    テストコードで動作確認、多分動いたはず

--- app/src/main/java/com/wrongwrong/experiment/FragmentShaders/NoiseGenerators.java
+++ app/src/main/java/com/wrongwrong/experiment/FragmentShaders/NoiseGenerators.java
@@ -1,10 +1,16 @@
 package com.wrongwrong.experiment.FragmentShaders;
 
 public class NoiseGenerators {
-    //引数として差とrgbを受け取り、正規分布で絶対値を取った値をノイズとして返す
+    //正規分布で絶対値を取った値をノイズとして返す
     //テストとして分散は0.3とする
     public static final String abs_normal_noise =
-            "float abs_normal_noise(vec3 rgb){" +
+            "float abs_normal_noise(vec3 rgb) {" +
                     "  return abs(normal_distribution(0.0, 0.3, vec2(rgb.x + rgb.z, rgb.x)));" +
+                    "}";
+
+    //指数分布、テストでラムダは0.2
+    public static final String exponential_noise =
+            "float exponential_noise(vec3 rgb) {" +
+                    " return exponential_distribution(0.2, vec2(rgb.x + rgb.z, rgb.x));" +
                     "}";
 }
