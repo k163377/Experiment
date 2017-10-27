cf367f7e6f8a3c266e402315da5dbce82f9e7c00
k163377 <k163377@edu.tut.ac.jp>
2017-10-27 17:42:21 +0900

    特に変更なし

--- app/src/main/java/com/wrongwrong/experiment/FragmentShaders/TestNoiseShader.java
+++ app/src/main/java/com/wrongwrong/experiment/FragmentShaders/TestNoiseShader.java
@@ -8,8 +8,8 @@ public class TestNoiseShader {
             "  vec4 v = texture2D(texture, fitting(texcoordVarying));" +
             "  float Pnoise = abs(calc_LL1(v.xyz))/1.297286;" +
             "  if(Pnoise > rand(vec2(v.x + v.y, v.y + v.z))) {" +
-            //"    gl_FragColor = vec4(0.0, exponential_noise(v.xyz), 0.0, 0.0);" +
-            "    gl_FragColor = vec4(0.0, abs_normal_noise(v.xyz), 0.0, 0.0);" +
+            "    gl_FragColor = vec4(0.0, exponential_noise(v.xyz), 0.0, 0.0);" +
+            //"    gl_FragColor = vec4(0.0, abs_normal_noise(v.xyz), 0.0, 0.0);" +
             "  } else gl_FragColor = vec4(0.0);\n" +
             "}\n";
 }
