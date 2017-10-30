ce87a1fd51e3f10aafd0c4ac23b08d7a000a4fc4
k163377 <k163377@edu.tut.ac.jp>
2017-10-30 15:54:44 +0900

    一旦作ってたノイズ削除 定数ノイズ作成

--- app/src/main/java/com/wrongwrong/experiment/FragmentShaders/NoiseGenerators.java
+++ app/src/main/java/com/wrongwrong/experiment/FragmentShaders/NoiseGenerators.java
@@ -1,16 +1,21 @@
 package com.wrongwrong.experiment.FragmentShaders;
 
 public class NoiseGenerators {
-    //正規分布で絶対値を取った値をノイズとして返す
-    //テストとして分散は0.3とする
-    public static final String abs_normal_noise =
-            "float abs_normal_noise(vec3 rgb) {" +
-                    "  return abs(normal_distribution(0.0, 0.3, vec2(rgb.x + rgb.z, rgb.x)));" +
-                    "}";
+    //メイン関数で利用する関数等を纏めたもの
+    public static final String mainHeader =
+            "#extension GL_OES_EGL_image_external : require\n" +
+                    "precision highp float;\n" +
+                    "varying vec2 texcoordVarying;\n" +
+                    "uniform samplerExternalOES texture;\n";
 
-    //指数分布、テストでラムダは0.2
-    public static final String exponential_noise =
-            "float exponential_noise(vec3 rgb) {" +
-                    "  return exponential_distribution(5.0, vec2(rgb.x + rgb.z, rgb.x));" +
-                    "}";
+    //強度に合わせたノイズを掛ける
+    public static final String constNoiseP = mainHeader+
+            OtherFuncs.calc_LL1+
+            "vec3 ConstNoiseP(vec3 rgb, float level){" +
+            "  float Pnoise = abs(calc_LL1(rgb))/1.297286; +\n" +
+            "  if(Pnoise > rand(vec2(rgb.x + rgb.y, rgb.y + rgb.z))) {\n" +
+            "    rgb = vec3(Pnoise * level);\n"+
+            "  } else rgb = vec3(0.0);\n" +
+            "  return rgb;\n" +
+            "}\n";
 }
