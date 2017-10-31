7fc036a502338de50e79ef8e4682dc4c72ae1384
k163377 <k163377@edu.tut.ac.jp>
2017-10-31 17:15:02 +0900

    とりあえずEnumは完成

--- app/src/main/java/com/wrongwrong/experiment/Enums.java
+++ app/src/main/java/com/wrongwrong/experiment/Enums.java
@@ -1,7 +1,31 @@
 package com.wrongwrong.experiment;
 
+import com.wrongwrong.experiment.FragmentShaders.ConstNoisePShaders;
+import com.wrongwrong.experiment.FragmentShaders.TestNoiseShader;
+
+import java.util.EnumMap;
+
 public enum Enums {
-    Constant,
+    TestNoise,
+    ConstantPNoise6,
+    ConstantPNoise8,
+    ConstantPNoise10;
 
+    public static final EnumMap<Enums, String> ShaderTitle;
+    static{
+        ShaderTitle = new EnumMap<Enums, String>(Enums.class);
+        ShaderTitle.put(Enums.TestNoise, "TestNoise");
+        ShaderTitle.put(Enums.ConstantPNoise6, "ConstantPNoise6");
+        ShaderTitle.put(Enums.ConstantPNoise8, "ConstantPNoise8");
+        ShaderTitle.put(Enums.ConstantPNoise10, "ConstantPNoise10");
+    }
 
+    public static final EnumMap<Enums, String> Shader;
+    static{
+        Shader = new EnumMap<Enums, String>(Enums.class);
+        Shader.put(Enums.TestNoise, TestNoiseShader.testNoise);
+        Shader.put(Enums.ConstantPNoise6, ConstNoisePShaders.ConstNoiseP6);
+        Shader.put(Enums.ConstantPNoise8, ConstNoisePShaders.ConstNoiseP8);
+        Shader.put(Enums.ConstantPNoise10, ConstNoisePShaders.ConstNoiseP10);
+    }
 }
