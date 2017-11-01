1a37f405daf72906cbae238f1f144ec795871dda
k163377 <k163377@edu.tut.ac.jp>
2017-11-01 18:38:54 +0900

    比例ノイズ追加

---
+++ app/src/main/java/com/wrongwrong/experiment/FragmentShaders/ProportionNoisePShaders.java
@@ -0,0 +1,17 @@
+package com.wrongwrong.experiment.FragmentShaders;
+
+public class ProportionNoisePShaders {
+    public static final String ProportionNoiseP10_0 = NoiseGenerators.proportionNoiseP+
+            "void main() {\n" +
+            "  vec4 v = texture2D(texture, fitting(texcoordVarying));" +
+            "  v.xyz = ProportionNoiseP(v.xyz, 1.0, 0.0);" +
+            "  gl_FragColor = v;" +
+            "}\n";
+
+    public static final String ProportionNoiseP5_5 = NoiseGenerators.proportionNoiseP+
+            "void main() {\n" +
+            "  vec4 v = texture2D(texture, fitting(texcoordVarying));" +
+            "  v.xyz = ProportionNoiseP(v.xyz, 0.5, 0.5);" +
+            "  gl_FragColor = v;" +
+            "}\n";
+}
