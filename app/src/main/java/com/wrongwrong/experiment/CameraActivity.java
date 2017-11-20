0e69ab05dc66c81062e4be9e561e6578f96be551
k163377 <boranti1995@gmail.com>
2017-11-20 21:55:28 +0900

    ちゃんとカメラ切り替えられるかなと思ったけど無理だった

--- app/src/main/java/com/wrongwrong/experiment/CameraActivity.java
+++ app/src/main/java/com/wrongwrong/experiment/CameraActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -24,7 +25,7 @@ public class CameraActivity extends Activity{
         glsv.setRenderer(r);
 
         setContentView(glsv);
-        
+
         //上から
         //ナビゲーションバーを隠す
         //一定時間経過で再度隠す
@@ -38,18 +39,30 @@ public class CameraActivity extends Activity{
 
     @Override
     protected void onPause() {
-        glsv.onPause();
+        if(null != glsv) glsv.onPause();
         super.onPause();
     }
 
     @Override
     protected void onResume() {
-        glsv.onResume();
+        if(null != glsv) glsv.onResume();
         super.onResume();
     }
 
     @Override
     protected void onDestroy(){
+        if(null != glsv) glsv = null;
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            //nullを代入してActivityを終了
+            if(null != glsv) glsv = null;
+            this.finish();
+            return true;
+        }
+        return false;
     }
 }
